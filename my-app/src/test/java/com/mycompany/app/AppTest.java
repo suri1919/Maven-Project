3cd6d882f50ceac348ac9ad088adb5fe411c6b7c
surendra <uggamsurendra@gmail.com>
2023-05-30 14:01:11 +0530

    my-app/

---
+++ my-app/src/test/java/com/mycompany/app/AppTest.java
@@ -0,0 +1,21 @@
+package com.mycompany.app;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * java tetsing for sample poll scm
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
